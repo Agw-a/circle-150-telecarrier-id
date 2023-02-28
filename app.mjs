fca494ae75aef99f27da9e943b16997c51faec15
A-gwa <89134520+Agw-a@users.noreply.github.com>
2023-02-28 08:50:29 +0000

    Styling for smaller screens

--- app.mjs
+++ app.mjs
@@ -4,12 +4,11 @@ function startApp() {
   // so create and use/call additional functions from here
 
   // pls remove the below and make some magic in here!
-// const reset=document.getElementById('');
-  // reset.onclick=window.l
+// const reset=document.getElementById('reset');
+//   reset.onclick=window.location.reload();
   const val = document.getElementById('val');
   val.onclick = function(e) {
     e.preventDefault();
-
     const phone = document.getElementById("phone");
     if (String(phone.value).length < 7) {
 
